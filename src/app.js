2592e42c5b89a7d42998eaca95095c09c746c85b
Ikeno Masaki <leafclover64@gmail.com>
2017-12-23 00:33:41 +0900

    connect twitter

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const Discord = require('discord.js');
+const Twitter = require('twitter');
 const client = new Discord.Client();
 
-// Tokenの取得
-const token = process.env.DISCORD_TOKEN;
+// Token等の取得
+const token = process.env.CM_DISCORD_TOKEN;
+var twClient = new Twitter({
+    consumer_key: process.env.CM_TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.CM_TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.CM_TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.CM_TWITTER_ACCESS_SECRET
+});
 
 client.on('ready', () => {
     console.log('I am ready');
 });
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
+    const text = newMember.displayName + "さんが入室しました";
+    twClient.post("statuses/update", {status: text}, (err, tweet, res) => {
+        console.log("tweet: "+text);
+    })
     console.log(newMember.displayName);
 });
 
